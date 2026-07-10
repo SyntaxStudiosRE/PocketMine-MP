345c1eec800f2233f00c34b37b1da5626e5c3672
GiovanySosa <dev4@pragfor.com>
2026-07-10 13:46:01 -0600

    Rebrand server /about output as SyntaxStudios-PocketMine-MP
    Changes VersionInfo::NAME (shown by /about and /version in-game) and
    GITHUB_URL (used in update-check messages) to point to our own fork
    instead of NetherGamesMC/pmmp.

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -30,11 +30,11 @@ use function is_int;
 use function str_repeat;
 
 final class VersionInfo{
-	public const NAME = "NG-PocketMine-MP";
+	public const NAME = "SyntaxStudios-PocketMine-MP";
 	public const BASE_VERSION = "5.44.2";
 	public const IS_DEVELOPMENT_BUILD = true;
 	public const BUILD_CHANNEL = "stable";
-	public const GITHUB_URL = "https://github.com/pmmp/PocketMine-MP";
+	public const GITHUB_URL = "https://github.com/SyntaxStudiosRE/PocketMine-MP";
 
 	/**
 	 * PocketMine-MP-specific version ID for world data. Used to determine what fixes need to be applied to old world
