29171d32e37613184ff91130b91152d22d9db145
GiovanySosa <dev4@pragfor.com>
2026-07-10 13:53:05 -0600

    Fix InstalledVersions lookup after composer package rename
    src/PocketMine.php checked InstalledVersions::getReference() against
    the hardcoded package name 'nethergamesmc/pocketmine-mp', which threw
    OutOfBoundsException once composer.json's own name was changed to
    syntaxstudiosre/pocketmine-mp. Update the lookup to match.

--- src/PocketMine.php
+++ src/PocketMine.php
@@ -268,7 +268,7 @@ JIT_WARNING
 		}
 		require_once($bootstrap);
 
-		$composerGitHash = InstalledVersions::getReference('nethergamesmc/pocketmine-mp');
+		$composerGitHash = InstalledVersions::getReference('syntaxstudiosre/pocketmine-mp');
 		if($composerGitHash !== null){
 			//we can't verify dependency versions if we were installed without using git
 			$currentGitHash = explode("-", VersionInfo::GIT_HASH(), 2)[0];
